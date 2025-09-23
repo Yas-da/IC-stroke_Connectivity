d13c6b700fd7d98ac8424db6b923a1d13e8d1648
Yas-da <yasmined2022@gmail.com>
2025-09-23 12:22:13 +0200

    Update Pre&post-Lesion_Connectivity_analysis.py

--- Pre&post-Lesion_Connectivity_analysis.py
+++ Pre&post-Lesion_Connectivity_analysis.py
@@ -1,25 +1,9 @@
-PS W:\Students\Yasmine\Projet\Connectivity_YD> & C:/Users/Phoenix/Anaconda3/envs/mediapipe-env/pytho
-n.exe w:/Students/Yasmine/Projet/Connectivity_YD/Pre_Post_lesion_connectivity/Pre_post_lesion_connec
-tivity.py
-Traceback (most recent call last):
-  File "w:\Students\Yasmine\Projet\Connectivity_YD\Pre_Post_lesion_connectivity\Pre_post_lesion_conn
-ectivity.py", line 101, in <module>
-    R = np.corrcoef(env_ds, rowvar=True)
-  File "C:\Users\Phoenix\Anaconda3\envs\mediapipe-env\lib\site-packages\numpy\lib\function_base.py",
- line 2889, in corrcoef
-    c = cov(x, y, rowvar, dtype=dtype)
-  File "C:\Users\Phoenix\Anaconda3\envs\mediapipe-env\lib\site-packages\numpy\lib\function_base.py",
- line 2747, in cov
-    c = dot(X, X_T.conj())
-numpy.core._exceptions._ArrayMemoryError: Unable to allocate 717. GiB for an array with shape (31029
-4, 310294) and data type float64
 import os, glob, h5py
 import numpy as np
 import scipy.signal as sp
 from scipy.stats import ttest_ind
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from scipy.ndimage import gaussian_filter
 
 # Directories
 base_dir = r'\\bigdata\Science\Med\Physiology\PTN\Yasmine\IC-stroke_connectivity\data'
@@ -36,14 +20,14 @@ bands = {
     'highgamma': (70,200)
 }
 filt_order = 4
-date_cutoff = 20250811  # Date of lesion
+date_cutoff = 20250811  # Date of the lesion
 
 grid_side = 8
 coords = [(i,j) for i in range(grid_side) for j in range(grid_side)]
 
 def load_trial_mat(filepath):
     with h5py.File(filepath,'r') as f:
-        signals = np.array(f['signals'])      # (nChan, nTime)
+        signals = np.array(f['signals'])      #(nChan, nTime)
         time_ecog = np.array(f['time_ecog']).squeeze()
         chan_labels = []
         for ref in f['channel_labels'][0]:
@@ -80,7 +64,6 @@ def cluster_permutation(Z_pre, Z_post, n_perm=1000, alpha=0.05):
     sig_init = pvals < alpha
     cluster_stat = np.abs(obs_diff) * sig_init
     obs_cluster_mass = cluster_stat.sum()
-
     all_data = np.hstack([Z_pre, Z_post])
     nA = Z_pre.shape[1]
     null_dist = []
@@ -105,14 +88,16 @@ for sess in sess_dirs:
     trial_dirs = glob.glob(os.path.join(sess,'trial*','MatFiles','*_allChannels.mat'))
     for trial_file in trial_dirs:
         dat = load_trial_mat(trial_file)
-        X = dat['signals']  # (nChan,nTime)
+        X = dat['signals']  #(nChan,nTime)
         nChan,nTime = X.shape
         condition = 'pre' if sess_date<date_cutoff else 'post'
         for bname,fr in bands.items():
             Xf = bandpass(X, fs, fr, order=filt_order)
             env = np.abs(sp.hilbert(Xf,axis=-1))
             env_ds = env[:,::10]   # downsample ~200 Hz
-            # Corrélation entre canaux, 67x67
+            # Ensure correct shape for correlation (channels x time)
+            if env_ds.shape[0] != nChan:
+                env_ds = env_ds.T
             R = np.corrcoef(env_ds, rowvar=True)
             all_results[bname].append(dict(R=R, condition=condition,
                                            session=sess_name, trial=dat['metadata']['trial']))
@@ -125,13 +110,10 @@ for bname in bands:
     Rs_post= [r['R'] for r in all_results[bname] if r['condition']=='post']
     Rm_pre = np.mean(Rs_pre,axis=0)
     Rm_post= np.mean(Rs_post,axis=0)
-
     iu = np.triu_indices(Rm_pre.shape[0],1)
     ZA = np.array([fisher_z(R[iu]) for R in Rs_pre]).T
     ZB = np.array([fisher_z(R[iu]) for R in Rs_post]).T
-
     sig_mask, pvals = cluster_permutation(ZA,ZB)
-
     sigMat = np.zeros_like(Rm_pre,dtype=bool)
     sigMat[iu] = sig_mask
     sigMat = sigMat|sigMat.T
@@ -149,14 +131,14 @@ for bname in bands:
 pdf.close()
 print("PDF saved at:", pdf_path)
 
-# Features pour decoder
+# Features extraction
 features = []
 labels = []
 for bname in bands:
     for r in all_results[bname]:
         R = r['R']
         node_strength = R.sum(1)
-        bandpower = np.diag(R)
+        bandpower = np.diag(R)  
         feat = np.concatenate([node_strength, bandpower])
         features.append(feat)
         labels.append(0 if r['condition']=='pre' else 1)
