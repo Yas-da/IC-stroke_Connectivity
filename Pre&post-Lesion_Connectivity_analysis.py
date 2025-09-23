b40c78ec51681b94ca7d94322aea73f8de2a2b40
Yas-da <yasmined2022@gmail.com>
2025-09-23 12:19:34 +0200

    Update Pre&post-Lesion_Connectivity_analysis.py

--- Pre&post-Lesion_Connectivity_analysis.py
+++ Pre&post-Lesion_Connectivity_analysis.py
@@ -1,32 +1,17 @@
-PS W:\Students\Yasmine\Projet\Connectivity_YD> & C:/Users/Phoenix/Anaconda3/envs/mediapipe-env/pytho
-n.exe w:/Students/Yasmine/Projet/Connectivity_YD/Pre_Post_lesion_connectivity/Pre_post_lesion_connec
-tivity.py
-Traceback (most recent call last):
-  File "w:\Students\Yasmine\Projet\Connectivity_YD\Pre_Post_lesion_connectivity\Pre_post_lesion_conn
-ectivity.py", line 84, in <module>
-    R = np.corrcoef(env_ds) if nChan<=100 else np.corrcoef(env_ds[:nChan,:])
-  File "C:\Users\Phoenix\Anaconda3\envs\mediapipe-env\lib\site-packages\numpy\lib\function_base.py",
- line 2889, in corrcoef
-    c = cov(x, y, rowvar, dtype=dtype)
-  File "C:\Users\Phoenix\Anaconda3\envs\mediapipe-env\lib\site-packages\numpy\lib\function_base.py",
- line 2747, in cov
-    c = dot(X, X_T.conj())
-numpy.core._exceptions._ArrayMemoryError: Unable to allocate 717. GiB for an array with shape (31029
-4, 310294) and data type float64
-
 import os, glob, h5py
 import numpy as np
 import scipy.signal as sp
 from scipy.stats import ttest_ind
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from scipy.ndimage import gaussian_filter
 
 # Directories
 base_dir = r'\\bigdata\Science\Med\Physiology\PTN\Yasmine\IC-stroke_connectivity\data'
 out_dir  = r'\\bigdata\Science\Med\Physiology\PTN\Yasmine\IC-stroke_connectivity\ConnectivityResults'
 os.makedirs(out_dir, exist_ok=True)
 
-fs = 2000
+fs = 2000  # sampling frequency
 bands = {
     'delta': (1,4),
     'theta': (4,8),
@@ -36,19 +21,31 @@ bands = {
     'highgamma': (70,200)
 }
 filt_order = 4
-date_cutoff = 20250811
+date_cutoff = 20250811  # Date of lesion
+
+grid_side = 8
+coords = [(i,j) for i in range(grid_side) for j in range(grid_side)]
 
 def load_trial_mat(filepath):
     with h5py.File(filepath,'r') as f:
-        signals = np.array(f['signals'])
+        signals = np.array(f['signals'])      # (nChan, nTime)
         time_ecog = np.array(f['time_ecog']).squeeze()
         chan_labels = []
         for ref in f['channel_labels'][0]:
             chan_labels.append(''.join(chr(c[0]) for c in f[ref][:]))
-        meta = {k: f['metadata'][k][()] for k in f['metadata'].keys()}
+        meta = {}
+        for k in f['metadata'].keys():
+            try:
+                val = f['metadata'][k][()]
+                if hasattr(val,'tolist'):
+                    val = val.tolist()
+                meta[k] = val
+            except:
+                pass
         kin_x = np.array(f['kin_x']) if 'kin_x' in f else None
         time_vicon = np.array(f['time_vicon']) if 'time_vicon' in f else None
-    return dict(signals=signals, time=time_ecog, chan_labels=chan_labels, metadata=meta, kin_x=kin_x, time_vicon=time_vicon)
+    return dict(signals=signals, time=time_ecog, chan_labels=chan_labels,
+                metadata=meta, kin_x=kin_x, time_vicon=time_vicon)
 
 def bandpass(data, fs, frange, order=4):
     Wn = [frange[0]/(fs/2), frange[1]/(fs/2)]
@@ -58,6 +55,9 @@ def bandpass(data, fs, frange, order=4):
 def fisher_z(r):
     return np.arctanh(np.clip(r,-0.999999,0.999999))
 
+def inverse_fisher(z):
+    return np.tanh(z)
+
 def cluster_permutation(Z_pre, Z_post, n_perm=1000, alpha=0.05):
     nEdges = Z_pre.shape[0]
     obs_diff = Z_post.mean(1) - Z_pre.mean(1)
@@ -65,6 +65,7 @@ def cluster_permutation(Z_pre, Z_post, n_perm=1000, alpha=0.05):
     sig_init = pvals < alpha
     cluster_stat = np.abs(obs_diff) * sig_init
     obs_cluster_mass = cluster_stat.sum()
+
     all_data = np.hstack([Z_pre, Z_post])
     nA = Z_pre.shape[1]
     null_dist = []
@@ -89,16 +90,17 @@ for sess in sess_dirs:
     trial_dirs = glob.glob(os.path.join(sess,'trial*','MatFiles','*_allChannels.mat'))
     for trial_file in trial_dirs:
         dat = load_trial_mat(trial_file)
-        X = dat['signals']  #(nChan,nTime)
+        X = dat['signals']  # (nChan,nTime)
         nChan,nTime = X.shape
         condition = 'pre' if sess_date<date_cutoff else 'post'
         for bname,fr in bands.items():
             Xf = bandpass(X, fs, fr, order=filt_order)
             env = np.abs(sp.hilbert(Xf,axis=-1))
-            env_ds = env[:,::10]  # downsample ~200 Hz
-            # calcul corr 67x67
-            R = np.corrcoef(env_ds) if nChan<=100 else np.corrcoef(env_ds[:nChan,:])
-            all_results[bname].append(dict(R=R, condition=condition, session=sess_name, trial=dat['metadata']['trial']))
+            env_ds = env[:,::10]   # downsample ~200 Hz
+            # Corrélation entre canaux, 67x67
+            R = np.corrcoef(env_ds, rowvar=True)
+            all_results[bname].append(dict(R=R, condition=condition,
+                                           session=sess_name, trial=dat['metadata']['trial']))
 
 pdf_path = os.path.join(out_dir,"Connectivity_results.pdf")
 pdf = PdfPages(pdf_path)
@@ -108,13 +110,17 @@ for bname in bands:
     Rs_post= [r['R'] for r in all_results[bname] if r['condition']=='post']
     Rm_pre = np.mean(Rs_pre,axis=0)
     Rm_post= np.mean(Rs_post,axis=0)
+
     iu = np.triu_indices(Rm_pre.shape[0],1)
     ZA = np.array([fisher_z(R[iu]) for R in Rs_pre]).T
     ZB = np.array([fisher_z(R[iu]) for R in Rs_post]).T
+
     sig_mask, pvals = cluster_permutation(ZA,ZB)
+
     sigMat = np.zeros_like(Rm_pre,dtype=bool)
     sigMat[iu] = sig_mask
     sigMat = sigMat|sigMat.T
+
     fig,axs = plt.subplots(2,2,figsize=(12,10))
     im0=axs[0,0].imshow(Rm_pre,vmin=0,vmax=1,cmap='viridis'); axs[0,0].set_title(f'{bname} PRE')
     fig.colorbar(im0,ax=axs[0,0])
@@ -124,9 +130,11 @@ for bname in bands:
     fig.colorbar(im2,ax=axs[1,0])
     im3=axs[1,1].imshow(sigMat,cmap='gray'); axs[1,1].set_title('Significant edges')
     pdf.savefig(fig); plt.close(fig)
+
 pdf.close()
 print("PDF saved at:", pdf_path)
 
+# Features pour decoder
 features = []
 labels = []
 for bname in bands:
@@ -139,5 +147,6 @@ for bname in bands:
         labels.append(0 if r['condition']=='pre' else 1)
 features = np.array(features)
 labels = np.array(labels)
-np.savez(os.path.join(out_dir,"features_for_decoder.npz"), X=features, y=labels, bandnames=list(bands.keys()))
+np.savez(os.path.join(out_dir,"features_for_decoder.npz"),
+         X=features,y=labels,bandnames=list(bands.keys()))
 print("NPZ features saved.")
